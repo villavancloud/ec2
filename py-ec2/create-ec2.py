a0b3ec579e1d188c50a186f1b663ef995adfdc7b
Vimal <vvillavankothai@apple.com>
2018-12-06 16:23:00 -0800

    fixing python code

--- py-ec2/create-ec2.py
+++ py-ec2/create-ec2.py
@@ -7,7 +7,7 @@ vpc.create_tags(Tags=[{"Key": "Name", "Value": "default_vpc"}])
 vpc.wait_until_available()
 
 # create security group
-sec-group = ec2.create_security_group(
+secgroup = ec2.create_security_group(
     GroupName='sec-group1',
     Description='new security group',
     VpcId=vpc.id
@@ -18,20 +18,17 @@ ig = ec2.create_internet_gateway()
 vpc.attach_internet_gateway(InternetGatewayId=ig.id)
 
 # create route table
-route-table = vpc.create_route_table()
-route = route_table.create_route(
+routetable = vpc.create_route_table()
+route = routetable.create_route(
     DestinationCidrBlock='0.0.0.0/0',
     GatewayId=ig.id
 )
 
-
 # create subnet
 subnet = ec2.create_subnet(CidrBlock='192.168.1.0/24', VpcId=vpc.id)
 
-
 # associate route table with subnet
-route-table.associate_with_subnet(SubnetId=subnet.id)
-
+routetable.associate_with_subnet(SubnetId=subnet.id)
 
 # create a new security instance
 instances = ec2.create_instances(
