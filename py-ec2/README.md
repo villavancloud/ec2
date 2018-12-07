abe9fd199baff6c205f5aa99bf5579cb9eb4df35
Vimal <vvillavankothai@apple.com>
2018-12-06 16:06:44 -0800

    adding another python script to create ec2 instance with vpc, sec group, subnet

--- py-ec2/README.md
+++ py-ec2/README.md
@@ -1,5 +1,8 @@
 prerequisites: Have awscli ((pip install) or download zip, unzip and run installer) and boto3 installed. also have the aws creds configured by running the command "aws configure".
 
+
+
+
 To create a new keypair run:
 
 python create-kp.py
@@ -8,4 +11,8 @@ chmod 400 ec2-keypair.pem
 
 To create EC2 instance(s) run:
 
-python create-ec2-instances.py
+python create-simple-ec2-instance.py
+
+To create an EC2 instance with vpc, subnet, security group run:
+
+python create-ec2.py
---
+++ py-ec2/create-ec2.py
@@ -0,0 +1,44 @@
+import boto3
+ec2 = boto3.resource('ec2', region_name='us-west-1')
+
+# create vpc
+vpc = ec2.create_vpc(CidrBlock='192.168.0.0/16')
+vpc.create_tags(Tags=[{"Key": "Name", "Value": "default_vpc"}])
+vpc.wait_until_available()
+
+# create security group
+sec-group = ec2.create_security_group(
+    GroupName='sec-group1',
+    Description='new security group',
+    VpcId=vpc.id
+)
+
+# create and attach InternetGateway
+ig = ec2.create_internet_gateway()
+vpc.attach_internet_gateway(InternetGatewayId=ig.id)
+
+# create route table
+route-table = vpc.create_route_table()
+route = route_table.create_route(
+    DestinationCidrBlock='0.0.0.0/0',
+    GatewayId=ig.id
+)
+
+
+# create subnet
+subnet = ec2.create_subnet(CidrBlock='192.168.1.0/24', VpcId=vpc.id)
+
+
+# associate route table with subnet
+route-table.associate_with_subnet(SubnetId=subnet.id)
+
+
+# create a new security instance
+instances = ec2.create_instances(
+    ImageId='ami-011b6930a81cd6aaf',
+    InstanceType='t2.micro',
+    MaxCount=1,
+    MinCount=1,
+    NetworkInterfaces=[{'SubnetId': subnet.id, 'DeviceIndex': 0, 'AssociatePublicIpAddress': True, 'Groups': [sec_group.group_id]}]
+)
+instances[0].wait_until_running()
