74e9e33d11d8c0fdb9a8a9d5c9379cc19b2e0a3c
Vimal <vvillavankothai@apple.com>
2018-12-12 15:03:38 -0800

    modifying bash/create-ec2-instance.bash

--- bash/create-ec2-instance.bash
+++ bash/create-ec2-instance.bash
@@ -8,4 +8,4 @@ chmod 400 bashKeyPair.pem
 
 IMAGE_AMI_ID=$(aws ec2 describe-images --owners amazonaws ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.????????-x86_64-gp2' 'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
 
-aws ec2 run-instances --image-id $IMAGE_AMI_ID --count 1 --instance-type t1.micro --key-name bashKeyPair
+aws ec2 run-instances --image-id $IMAGE_AMI_ID --count 1 --instance-type t1.micro --key-name bashKeyPair --tag-specifications 'ResourceType=instance, Tags=[{Key=Name,Value=made-in-bash}]'
