9cb42ec33451ce29bfacacdb4cb041db22c164a7
Vimal <vvillavankothai@apple.com>
2018-12-12 14:47:35 -0800

    adding bash/create-ec2-instance.bash

--- bash/create-ec2-instance.bash
+++ bash/create-ec2-instance.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-ec2-authorize default -P tcp -p 22 -s 0.0.0.0/0
+ec2 authorize default -P tcp -p 22 -s 0.0.0.0/0
 
-aws ec2 create-key-pair --key-name bashKeyPair > bashKeyPair.pem
+aws ec2 create-key-pair --key-name bashKeyPair --output json | jq -r '.KeyMaterial' > bashKeyPair.pem
 
 chmod 400 bashKeyPair.pem
 
-aws ec2 run-instances --image-id ami-011b6930a81cd6aaf --count 1 --instance-type t1.micro --key-name bashKeyPair
+IMAGE_AMI_ID=$(aws ec2 describe-images --owners amazonaws ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.????????-x86_64-gp2' 'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
+
+aws ec2 run-instances --image-id $IMAGE_AMI_ID --count 1 --instance-type t1.micro --key-name bashKeyPair
